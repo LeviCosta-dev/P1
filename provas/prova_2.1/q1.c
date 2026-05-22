e149b8484b9d525ee0017349f5dfa77828d7763b
Levi Costa <leviccosta.dev@gmail.com>
2026-05-22 15:35:24 -0300

    Prova

--- provas/prova_2.1/q1.c
+++ provas/prova_2.1/q1.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+void contar_fibonacci(long long int arr[], int n, int i)
+{
+    if (i > n)
+    {
+        printf("%lld\n", arr[i-1]);
+        return;
+    }
+
+    if (i == 0)
+    {
+        arr[i] = 0;
+    }
+
+    else if (i == 1)
+    {
+        arr[i] = 1;
+    }
+
+    else
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+
+    contar_fibonacci(arr, n, i + 1);
+}
+
 int main(void)
 {
+    int n;
+    scanf("%d", &n);
 
+    long long int arr[n];
+
+    contar_fibonacci(arr, n, 0);
+
+
+    return 0;
 }
--- provas/prova_2.1/q2.c
+++ provas/prova_2.1/q2.c
@@ -1,6 +1,67 @@
 #include <stdio.h>
 
+int contar_isolados(int arr[], int n, int i, int count)
+{
+    if (i == n - 1)
+    {
+        if (arr[i] > arr[0] && arr[i] > arr[i-1])
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    else
+    {
+        if (i == 0)
+        {
+            if (arr[i] > arr[i + 1] && arr[i] > arr[n-1])
+            {
+                count++;
+            }
+        }
+
+        else
+        {
+            if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1])
+            {
+                count++;
+            }
+        }
+
+        return contar_isolados(arr, n, i + 1, count);
+    }
+}
+
+void ler(int arr[], int n, int i)
+{
+    if (i == n)
+    {
+        return;
+    }
+
+    else
+    {
+        scanf("%d", &arr[i]);
+
+        ler(arr, n, i + 1);
+    }
+}
+
 int main(void)
 {
-    
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    ler(arr, n, 0);
+
+    int isolados = contar_isolados(arr, n, 0, 0);
+
+    printf("%d\n", isolados);
+
+
+    return 0;
 }
